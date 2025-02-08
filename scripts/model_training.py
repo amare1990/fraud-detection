ce943252e5368440397191b8308f94f67f70c7fe
amare1990 <amaremek@gmail.com>
2025-02-08 21:50:33 +0300

    Implement a method to train and evaluate traditional ML models

--- scripts/model_training.py
+++ scripts/model_training.py
@@ -8,6 +8,13 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.neural_network import MLPClassifier
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report, confusion_matrix
+
 
 
 class FraudDetectionModel:
@@ -29,4 +36,46 @@ class FraudDetectionModel:
         )
 
         print("Data prepared: Train and test sets created.")
+
+
+    def train_sklearn_models(self):
+        models = {
+            "Logistic Regression": LogisticRegression(),
+            "Decision Tree": DecisionTreeClassifier(),
+            "Random Forest": RandomForestClassifier(),
+            "Gradient Boosting": GradientBoostingClassifier(),
+            "MLP Classifier": MLPClassifier(hidden_layer_sizes=(100,), max_iter=300)
+        }
+
+        perf_result = {}
+
+        for name, model in models.items():
+            print(f"Training {name}...")
+            model.fit(self.X_train, self.y_train)
+            self.models[name] = model
+
+            y_pred = model.predict(self.X_test)
+            accuracy = accuracy_score(self.y_test, y_pred)
+            precision = precision_score(self.y_test, y_pred, average='weighted')
+            recall = recall_score(self.y_test, y_pred, average='weighted')
+            f1 = f1_score(self.y_test, y_pred, average='weighted')
+            conf_matrix = confusion_matrix(self.y_test, y_pred)
+            class_report = classification_report(self.y_test, y_pred)
+
+            print(f"{name} Accuracy: {accuracy:.4f}")
+            print(class_report)
+
+            perf_result[name] = {
+                'accuracy': accuracy,
+                'precision': precision,
+                'recall': recall,
+                'f1_score': f1,
+                'conf_matrix': conf_matrix,
+                'class_report': class_report
+            }
+
+        return perf_result
+
+
+
 
