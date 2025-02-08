b1cf2d5dde4c356e57620380c4840d7d783afb3c
amare1990 <amaremek@gmail.com>
2025-02-08 21:07:28 +0300

    Create cnn.py script and add CNN model for 1D data classification in PyTorch

---
+++ scripts/cnn.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+import torch
+import torch.nn as nn
+
+
+# CNN Model in PyTorch
+class CNNModel(nn.Module):
+    def __init__(self, input_size):
+        super(CNNModel, self).__init__()
+        self.conv1 = nn.Conv1d(in_channels=1, out_channels=32, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool1d(kernel_size=2)
+        self.conv2 = nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(64 * (input_size // 2 // 2), 128)
+        self.fc2 = nn.Linear(128, 1)
+        self.sigmoid = nn.Sigmoid()
