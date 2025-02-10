d88e120433ed30de97403559b8115204228a1493
amare1990 <amaremek@gmail.com>
2025-02-10 22:42:29 +0300

    Implement a method to explain the model using SHAP values

--- scripts/model_explainability.py
+++ scripts/model_explainability.py
@@ -18,3 +18,25 @@ class ModelExplainability:
         self.X_train = X_train
         self.X_test = X_test
         self.feature_names = feature_names
+
+    ### SHAP EXPLAINABILITY ###
+    def shap_explain(self):
+        """
+        Explain the model using SHAP values.
+        """
+        print("Generating SHAP explanations...")
+        # Use SHAP KernelExplainer for black-box models
+        explainer = shap.Explainer(self.model.predict, self.X_train)
+        shap_values = explainer(self.X_test)
+
+        # Summary Plot: Shows global feature importance
+        shap.summary_plot(shap_values, self.X_test, feature_names=self.feature_names)
+
+        # Force Plot: Visualizes individual predictions
+        shap.initjs()
+        sample_index = 0
+        shap.force_plot(explainer.expected_value, shap_values[sample_index], self.X_test.iloc[sample_index, :]\
+                        , feature_names=self.feature_names)
+        # Dependence Plot: Shows how a single feature affects predictions
+        feature_index = 0
+        shap.dependence_plot(feature_index, shap_values, self.X_test, feature_names=self.feature_names)
