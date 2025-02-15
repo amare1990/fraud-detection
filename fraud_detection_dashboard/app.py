f7c75372c795e9258e015e57acb3365130030e7d
amare1990 <amaremek@gmail.com>
2025-02-15 15:26:47 +0300

    Implement Flask backend to serve fraud detection data through API endpoints

---
+++ fraud_detection_dashboard/app.py
@@ -0,0 +1,51 @@
+"""Flask backend to serve fraud detection data through API endpoints."""
+
+from flask import Flask, jsonify
+import pandas as pd
+
+
+app = Flask(__name__)
+
+# Load fraud data
+
+df = pd.read_csv("Fraud_Data.csv")
+
+
+@app.route("/", methods=["GET"])
+def home():
+  return jsonify({"message": "Fraud API running"}), 200
+
+
+@app.route("/summary", methods=["GER"])
+
+def summary():
+  total_purchasing = len(df)
+  total_fraud_cases = df[df["class"] == 1].shape[0]
+  fraud_percentage = (total_fraud_cases / total_purchasing) * 100
+
+  summary_data = {
+      "total_transactions": total_purchasing,
+      "total_fraud_cases": total_fraud_cases,
+      "fraud_percentage": round(fraud_percentage, 2),
+  }
+
+  return jsonify(summary_data)
+
+@app.route("/fraud_trend", methods=["GET"])
+def fraud_trend():
+    fraud_cases_per_day = df.groupby("date")["class"].sum().reset_index()
+    return jsonify(fraud_cases_per_day.to_dict(orient="records"))
+
+@app.route("/fraud_by_device", methods=["GET"])
+def fraud_by_device():
+    fraud_counts = df[df["class"] == 1]["device"].value_counts().to_dict()
+    return jsonify(fraud_counts)
+
+@app.route("/fraud_by_browser", methods=["GET"])
+def fraud_by_browser():
+    fraud_counts = df[df["class"] == 1]["browser"].value_counts().to_dict()
+    return jsonify(fraud_counts)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
