36a3b946c50c6b650b53a7605696a5da45d939f0
amare1990 <amaremek@gmail.com>
2025-02-16 13:26:49 +0300

    Update the dashboard to analyze fraud detection acroos geographical locations, fraud trends by day over week, hours by day

--- fraud_detection_dashboard/app.py
+++ fraud_detection_dashboard/app.py
@@ -7,9 +7,22 @@ import pandas as pd
 app = Flask(__name__)
 
 # Load fraud data
-
 df = pd.read_csv("Fraud_Data.csv")
 
+# Load the processed data to analyze frauds by geographical location
+df_processed = pd.read_csv("processed_data.csv")
+
+print(df_processed.columns)
+
+
+# fraud_counts = df_processed[df_processed["class"] == 1].iloc[:, 5:].sum().to_dict()
+
+# Ensure that the 'purchase_time' column is in datetime format
+df['purchase_time'] = pd.to_datetime(df['purchase_time'])
+
+# Extractin features
+df["hour_of_day"] = df["purchase_time"].dt.hour
+df["day_of_week"] = df["purchase_time"].dt.dayofweek
 
 @app.route("/", methods=["GET"])
 def home():
@@ -22,23 +35,43 @@ def summary():
   total_transaction = len(df)
   total_fraud_cases = df[df["class"] == 1].shape[0]
   fraud_percentage = (total_fraud_cases / total_transaction) * 100
+  total_features = list(df.columns)
 
   summary_data = {
       "total_transactions": total_transaction,
       "total_fraud_cases": total_fraud_cases,
       "fraud_percentage": round(fraud_percentage, 2),
+      "total_features": total_features,
   }
 
   return jsonify(summary_data)
 
+
+@app.route("/fraud_by_country", methods=["GET"])
+def fraud_by_country():
+    # fraud_counts = df_processed[df_processed["class"] == 1]["country"].value_counts().to_dict()
+    fraud_counts = df_processed[df_processed["class"] == 1].filter(like="country_").sum().to_dict()
+    return jsonify(fraud_counts)
+
+
 @app.route("/fraud_trend", methods=["GET"])
 def fraud_trend():
-    fraud_cases_per_day = df.groupby("date")["class"].sum().reset_index()
+    fraud_cases_per_day = df.groupby('purchase_time')["class"].sum().reset_index()
     return jsonify(fraud_cases_per_day.to_dict(orient="records"))
+
+@app.route("/fraud_trend_by_day", methods=["GET"])
+def fraud_trend_by_day():
+    fraud_cases_per_day = df.groupby("hour_of_day")["class"].sum().reset_index()
+    return jsonify(fraud_cases_per_day.to_dict(orient="records"))
+
+@app.route("/fraud_trend_by_week", methods=["GET"])
+def fraud_trend_by_week():
+    fraud_cases_per_week = df.groupby("day_of_week")["class"].sum().reset_index()
+    return jsonify(fraud_cases_per_week.to_dict(orient="records"))
 
 @app.route("/fraud_by_device", methods=["GET"])
 def fraud_by_device():
-    fraud_counts = df[df["class"] == 1]["device"].value_counts().to_dict()
+    fraud_counts = df[df["class"] == 1]["device_id"].value_counts().to_dict()
     return jsonify(fraud_counts)
 
 @app.route("/fraud_by_browser", methods=["GET"])
--- fraud_detection_dashboard/dashboard.py
+++ fraud_detection_dashboard/dashboard.py
@@ -16,13 +16,20 @@ app = dash.Dash(__name__)
 
 # Fetch data from the local server
 summary_data = requests.get("http://127.0.0.1:5000/summary").json()
+
+# Fetch fraud by country data
+fraud_by_country = requests.get("http://127.0.0.1:5000/fraud_by_country").json()
+fraud_by_country_df = pd.DataFrame(list(fraud_by_country.items()), columns=["country", "fraud_cases"])
+
+
 fraud_trend = pd.DataFrame(requests.get("http://127.0.0.1:5000/fraud_trend").json())
+fraud_trend_by_day = pd.DataFrame(requests.get("http://127.0.0.1:5000/fraud_trend_by_day").json())
+fraud_trend_by_week = pd.DataFrame(requests.get("http://127.0.0.1:5000/fraud_trend_by_week").json())
 fraud_by_device = requests.get("http://127.0.0.1:5000/fraud_by_device").json()
 fraud_by_browser = requests.get("http://127.0.0.1:5000/fraud_by_browser").json()
 
 
 # dashboard layout
-# Layout
 app.layout = html.Div(children=[
     html.H1("Fraud Detection Dashboard", style={"textAlign": "center"}),
 
@@ -31,7 +38,58 @@ app.layout = html.Div(children=[
         html.Div(f"Total Transactions: {summary_data['total_transactions']}", className="box"),
         html.Div(f"Total Fraud Cases: {summary_data['total_fraud_cases']}", className="box"),
         html.Div(f"Fraud Percentage: {summary_data['fraud_percentage']}%", className="box"),
+        html.Div(f"Total features: {summary_data['total_features']}", className="box"),
     ], className="summary-boxes"),
+
+
+    dcc.Graph(
+        figure=px.choropleth(
+            fraud_by_country_df,
+            locations="country",  # Country names
+            locationmode="country names",  # Ensure compatibility with country names
+            color="fraud_cases",  # Color intensity based on fraud cases
+            title="Geographic Distribution of Fraud Cases",
+            color_continuous_scale="Reds",
+            labels={"fraud_cases": "Fraud Cases"},
+        )
+    ),
+
+
+     # Fraud Trend Line Chart General
+    dcc.Graph(
+        figure=px.line(
+            fraud_trend,
+            x="purchase_time",
+            y="class",
+            title="Fraud Cases Over Time",
+            labels={"purchase_time": "Date", "class": "Number of Fraud Cases"},
+            markers=True
+        )
+    ),
+
+    # Fraud Trend Line Chart by Week
+    dcc.Graph(
+        figure=px.line(
+            fraud_trend_by_week,
+            x="day_of_week",
+            y="class",
+            title="Fraud Cases Over Week",
+            labels={"Day of week": "Day", "class": "Number of Fraud Cases"},
+            markers=True
+        )
+    ),
+
+    # # Fraud Trend Line Chart by Day
+    dcc.Graph(
+        figure=px.line(
+            fraud_trend_by_day,
+            x="hour_of_day",
+            y="class",
+            title="Fraud Cases Over day",
+            labels={"Hour of day": "Hour", "class": "Number of Fraud Cases"},
+            markers=True
+        )
+    ),
 
     # Fraud by Device Bar Chart
     dcc.Graph(
