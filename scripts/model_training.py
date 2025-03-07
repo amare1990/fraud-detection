7a40947242b7c7faeb6ea8a0662f3344fd94b1d6
amare1990 <amaremek@gmail.com>
2025-03-07 17:49:03 +0300

    Modularize the model_training.py module by writing a separate evaluation method and other utility methods

--- scripts/model_training.py
+++ scripts/model_training.py
@@ -1,45 +1,35 @@
-"""
-Model building building, training, evalauation.
-"""
+""" Build, train, and evaluation."""
+
 import pickle
-import random
-
-import pandas as pd
 import numpy as np
-
+import pandas as pd
 import matplotlib.pyplot as plt
-
 import torch
-from torch import nn
-from torch import optim
+from torch import nn, optim
 from torch.utils.data import DataLoader, TensorDataset
-
 from sklearn.model_selection import train_test_split
-
 from sklearn.linear_model import LogisticRegression
-
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.neural_network import MLPClassifier
-
-from sklearn.metrics import accuracy_score, precision_score, recall_score, \
-    f1_score, classification_report, confusion_matrix
-
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report, confusion_matrix
 import mlflow
 import mlflow.sklearn
 import mlflow.pytorch
 
+
+from scripts.smotified_gan_balancer import SMOTifiedGANBalancer
 from scripts.cnn import CNNModel
 from scripts.rnn import RNNModel
 from scripts.lstm import LSTMModel
 
-from scripts.smotified_gan_balancer import SMOTifiedGANBalancer
+BASE_DIR = "/home/am/Documents/Software Development/10_Academy Training/week_8-9/fraud-detection"
+
 
 
 # Set random seed for reproducibility
 def set_seed(seed_value=42):
-    """Set seed for reproducibility across all necessary libraries."""
-    random.seed(seed_value)
+    """Set seed for reproducibility across all necessary libraries."""-    random.seed(seed_value)
     np.random.seed(seed_value)
     torch.manual_seed(seed_value)
 
@@ -57,67 +47,52 @@ set_seed(42)
 
 
 class FraudDetectionModel:
-    """A Machine learning model using PyTorch."""
-
-    def __init__(self, processed_data_path, target_column):
-        """Initialize the class with data, target_column."""
-
-        self.data = pd.read_csv(processed_data_path)
-
+    def __init__(self, data, target_column):
+        self.data = data
         self.target_column = target_column
-
         self.balancer = SMOTifiedGANBalancer()
-
         self.models = {}
-
-        # Initialize dataset attributes
         self.X_train = None
         self.X_test = None
         self.y_train = None
         self.y_test = None
 
-    def retrieve_numerical_columns(self):
-        """Return a list of numerical columns."""
-        return self.data.select_dtypes(include=[np.number]).columns.tolist()
+
 
     def balance_data(self, target_col='class'):
-        """
-        Calls the SMOTified+GAN method to balance data.
-        """
+        print(f"\n\n{'*'*100}\n")
         print("Balancing data using SMOTified+GAN...")
 
-        X = self.data.values
+        # Features (X) and target (y) - drop the target column from X
+        X = self.data.drop(columns=[target_col]).values
         y = self.data[target_col].values
 
+        # Apply SMOTified+GAN for balancing the dataset
         X_balanced, y_balanced = self.balancer.balance_data(X, y)
 
-        # Store the balanced data
-        self.data = pd.DataFrame(X_balanced)
+        # Create a new DataFrame with the balanced data
+        self.data = pd.DataFrame(X_balanced, columns=self.data.drop(columns=[target_col]).columns)
         self.data[target_col] = y_balanced
 
-        # Save the balanced data
-        output_path = '/home/am/Documents/Software Development/10_Academy Training/\
-        week_8-9/fraud-detection/data/balanced_data.csv'
+        # Save the balanced data to a CSV file
+        output_path = f'{BASE_DIR}/data/balanced_processed_data.csv'
         self.data.to_csv(output_path, index=False)
-
+        print(f"Balanced and processed data saved as {output_path}")
         print("Data balancing completed successfully.")
-        print(
-            f"Balanced and processed data saved successfully as {output_path}")
+
 
     def data_preparation(self, test_size=0.2):
-        """Prepare data for training."""
+        print(f"\n\n{'*'*100}\n")
+        print("Data preparation starting...")
         X = self.data.drop(columns=[self.target_column])
         y = self.data[self.target_column].values
 
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
             X, y, test_size=test_size, random_state=42, stratify=y
         )
-
-        print("Data prepared for training: Train and test sets created.")
+        print("Data prepared: Train and test sets created.")
 
     def train_sklearn_models(self):
-        """Train traditional ML models.
-        """
         models = {
             "Logistic Regression": LogisticRegression(),
             "Decision Tree": DecisionTreeClassifier(),
@@ -126,94 +101,31 @@ class FraudDetectionModel:
             "MLP Classifier": MLPClassifier(hidden_layer_sizes=(100,), max_iter=300)
         }
 
-        perf_result = {}
-
         for name, model in models.items():
             print(f"Training {name}...")
             model.fit(self.X_train, self.y_train)
             self.models[name] = model
 
-            y_pred = model.predict(self.X_test)
-            accuracy = accuracy_score(self.y_test, y_pred)
-            precision = precision_score(
-                self.y_test, y_pred, average='weighted')
-            recall = recall_score(self.y_test, y_pred, average='weighted')
-            f1 = f1_score(self.y_test, y_pred, average='weighted')
-            conf_matrix = confusion_matrix(self.y_test, y_pred)
-            class_report = classification_report(self.y_test, y_pred)
-
-            # Print performance metrics
-            print(f"\n{name} Accuracy: {accuracy:.4f}")
-            print(f"\n{name} Precision: {precision:.4f}")
-            print(f"\n{name} Recall: {recall:.4f}")
-            print(f"\n{name} F1-Score: {f1:.4f}")
-
-            print(f"\n{name} Confusion Matrix:")
-            print("-" * 40)
-            print(conf_matrix)
-
-            print("-" * 40)
-            print(f"\n{name} class_report:")
-            print(class_report)
-
-            perf_result[name] = {
-                'accuracy': accuracy,
-                'precision': precision,
-                'recall': recall,
-                'f1_score': f1,
-                'conf_matrix': conf_matrix,
-                'class_report': class_report
-            }
-
-        return perf_result
-
-    def train_deep_learning_models(
-            self, model_type="LSTM", epochs=10, batch_size=32, learning_rate=0.001):
-        """
-        Training Deep Learning models.
-        """
-        X_train_tensor = torch.tensor(self.X_train, dtype=torch.float32)
-        X_test_tensor = torch.tensor(self.X_test, dtype=torch.float32)
-        y_train_tensor = torch.tensor(
-            self.y_train, dtype=torch.float32).unsqueeze(1)
-        y_test_tensor = torch.tensor(
-            self.y_test, dtype=torch.float32).unsqueeze(1)
-
-        # Reshape data to add sequence_length dimension
-        X_train_tensor = X_train_tensor.unsqueeze(1)
-        X_test_tensor = X_test_tensor.unsqueeze(1)
+    def train_deep_learning_models(self, model_type="LSTM", epochs=10, batch_size=32, learning_rate=0.001):
+        X_train_tensor = torch.tensor(self.X_train, dtype=torch.float32).unsqueeze(1)
+        X_test_tensor = torch.tensor(self.X_test, dtype=torch.float32).unsqueeze(1)
+        y_train_tensor = torch.tensor(self.y_train, dtype=torch.float32).unsqueeze(1)
+        y_test_tensor = torch.tensor(self.y_test, dtype=torch.float32).unsqueeze(1)
 
         train_dataset = TensorDataset(X_train_tensor, y_train_tensor)
         test_dataset = TensorDataset(X_test_tensor, y_test_tensor)
 
-        train_loader = DataLoader(
-            train_dataset,
-            batch_size=batch_size,
-            shuffle=True)
-
-        test_loader = DataLoader(
-            test_dataset,
-            batch_size=batch_size,
-            shuffle=True)
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+        test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
         input_size = self.X_train.shape[1]
-
-        # Initialize model based on model_type
-        if model_type == 'CNN':
-            model = CNNModel(input_size)
-        elif model_type == 'RNN':
-            model = RNNModel(input_size)
-        else:
-            model = LSTMModel(input_size)
+        model = self._initialize_model(model_type, input_size)
 
         criterion = nn.BCELoss()
         optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-        print(f'Training {model_type} Model...')
-
-        # Store losses for visualization
+        print(f"Training {model_type} Model...")
         losses = []
-
         for epoch in range(epochs):
             model.train()
             epoch_loss = 0
@@ -227,144 +139,136 @@ class FraudDetectionModel:
 
             avg_loss = epoch_loss / len(train_loader)
             losses.append(avg_loss)
+            print(f"Epoch {epoch + 1}/{epochs}, Loss: {avg_loss:.4f}")
 
-            print(
-                f"Epoch {epoch + 1}/{epochs}, Loss: {epoch_loss / len(train_loader):.4f}")
+        self._plot_loss_curve(losses, model_type, epochs)
+        self.models[model_type] = model
 
-        # Plot loss curve
+    def _initialize_model(self, model_type, input_size):
+        if model_type == "CNN":
+            return CNNModel(input_size)
+        elif model_type == "RNN":
+            return RNNModel(input_size)
+        else:
+            return LSTMModel(input_size)
+
+    def _plot_loss_curve(self, losses, model_type, epochs):
         plt.figure(figsize=(8, 6))
-        plt.plot(
-            range(
-                1,
-                epochs + 1),
-            losses,
-            marker='o',
-            linestyle='-',
-            color='b',
-            label=f'{model_type} Loss')
+        plt.plot(range(1, epochs + 1), losses, marker='o', linestyle='-', color='b', label=f'{model_type} Loss')
         plt.xlabel("Epochs")
         plt.ylabel("Loss")
         plt.title(f"{model_type} Training Loss Curve")
         plt.legend()
         plt.grid()
-
-        # Save the loss plot
-        loss_plot_path = f"/home/am/Documents/Software Development/10_Academy Training/\
-            week_8-9/fraud-detection/notebooks/plots/{model_type}_training_loss.png"
-        plt.savefig(loss_plot_path,
-                    dpi=300,
-                    bbox_inches='tight')
+        loss_plot_path = f"{BASE_DIR}/notebooks/plots/models/{model_type}_training_loss.png"
+        plt.savefig(loss_plot_path, dpi=300, bbox_inches='tight')
         print(f"Loss plot saved as {loss_plot_path}")
-
         plt.show()
 
-        # Model evaluation for deep learning
-        model.eval()
-        correct, total = 0, 0
-        predictions_list = []
-        true_labels_list = []
-
-        # Create a dictionary to store performance metrics
+    def evaluate_models(self):
         perf_result = {}
+        for name, model in self.models.items():
+            if isinstance(model, (LogisticRegression, DecisionTreeClassifier, RandomForestClassifier,
+                                  GradientBoostingClassifier, MLPClassifier)):
+                y_pred = model.predict(self.X_test)
+                accuracy = accuracy_score(self.y_test, y_pred)
+                precision = precision_score(self.y_test, y_pred, average='weighted')
+                recall = recall_score(self.y_test, y_pred, average='weighted')
+                f1 = f1_score(self.y_test, y_pred, average='weighted')
+                conf_matrix = confusion_matrix(self.y_test, y_pred)
+                class_report = classification_report(self.y_test, y_pred)
 
-        with torch.no_grad():
-            for batch_x, batch_y in test_loader:
-                outputs = model(batch_x)
-                predictions = (outputs > 0.5).float()
-                predictions_list.extend(
-                    predictions.cpu().numpy())  # Store predictions
-                true_labels_list.extend(
-                    batch_y.cpu().numpy())  # Store true labels
-                correct += (predictions == batch_y).sum().item()
-                total += batch_y.size(0)
+                print(f"\n{name} Metrics:")
+                print(f"Accuracy: {accuracy:.4f}")
+                print(f"Precision: {precision:.4f}")
+                print(f"Recall: {recall:.4f}")
+                print(f"F1-Score: {f1:.4f}")
+                print("\nConfusion Matrix:\n", conf_matrix)
+                print("\nClassification Report:\n", class_report)
 
-        accuracy = correct / total
+                perf_result[name] = {
+                    'accuracy': accuracy,
+                    'precision': precision,
+                    'recall': recall,
+                    'f1_score': f1,
+                    'conf_matrix': conf_matrix,
+                    'class_report': class_report
+                }
 
-        # Convert lists to arrays for metric calculations
-        predictions_array = np.array(predictions_list)
-        true_labels_array = np.array(true_labels_list)
+            if isinstance(model, nn.Module):
+                y_pred, true_labels = self._evaluate_model(model, self.X_test, self.y_test, name)
 
-        # Calculate performance metrics
-        precision = precision_score(
-            true_labels_array,
-            predictions_array,
-            average='weighted')
-        recall = recall_score(
-            true_labels_array,
-            predictions_array,
-            average='weighted')
-        f1 = f1_score(true_labels_array, predictions_array, average='weighted')
-        conf_matrix = confusion_matrix(true_labels_array, predictions_array)
-        class_report = classification_report(
-            true_labels_array, predictions_array)
+                accuracy = accuracy_score(true_labels, y_pred)
+                precision = precision_score(true_labels, y_pred, average='weighted')
+                recall = recall_score(true_labels, y_pred, average='weighted')
+                f1 = f1_score(true_labels, y_pred, average='weighted')
+                conf_matrix = confusion_matrix(true_labels, y_pred)
+                class_report = classification_report(true_labels, y_pred)
 
-        # Print metrics
-        print(f"\n{model_type} Accuracy: {accuracy:.4f}")
-        print(f"\n {model_type} Precision: {precision:.4f}")
-        print(f"\n {model_type} Recall: {recall:.4f}")
-        print(f"\n {model_type} F1-Score: {f1:.4f}")
+                print(f"\n{name} Metrics:")
+                print(f"Accuracy: {accuracy:.4f}")
+                print(f"Precision: {precision:.4f}")
+                print(f"Recall: {recall:.4f}")
+                print(f"F1-Score: {f1:.4f}")
+                print("\nConfusion Matrix:\n", conf_matrix)
+                print("\nClassification Report:\n", class_report)
 
-        # Print confusion matrix
-        print(f"\n{model_type} - Confusion Matrix:")
-        print("-" * 40)
-        print(conf_matrix)
-
-        # Print classification report
-        print(f"\n{model_type} - Classification Report:")
-        print("-" * 40)
-        print("Classification Report:")
-        print(class_report)
-
-        # Store the results in perf_result
-        perf_result[model_type] = {
-            'accuracy': accuracy,
-            'precision': precision,
-            'recall': recall,
-            'f1_score': f1,
-            'conf_matrix': conf_matrix,
-            'class_report': class_report
-        }
-
-        # Save the model
-        self.models[model_type] = model
+                perf_result[name] = {
+                    'accuracy': accuracy,
+                    'precision': precision,
+                    'recall': recall,
+                    'f1_score': f1,
+                    'conf_matrix': conf_matrix,
+                    'class_report': class_report
+                }
 
         return perf_result
 
+    def _evaluate_model(self, model, X_test, y_test, model_type):
+        model.eval()
+        correct, total = 0, 0
+        predictions_list, true_labels_list = [], []
+
+        with torch.no_grad():
+            inputs = torch.tensor(X_test, dtype=torch.float32).unsqueeze(1)
+            labels = torch.tensor(y_test, dtype=torch.float32).unsqueeze(1)
+
+            outputs = model(inputs)
+            predictions = (outputs > 0.5).float()
+
+            predictions_list.extend(predictions.cpu().numpy())
+            true_labels_list.extend(labels.cpu().numpy())
+            correct += (predictions == labels).sum().item()
+            total += labels.size(0)
+
+        accuracy = correct / total
+        predictions_array = np.array(predictions_list)
+        true_labels_array = np.array(true_labels_list)
+        return predictions_array, true_labels_array
+
+
     def save_model(self, model_name):
-        """Save the model given as an attribute.
-        """
         model = self.models.get(model_name)
         if not model:
             print(f'Model {model_name} not found!')
             return
 
         if model_name in ['CNN', 'RNN', 'LSTM']:
-            torch.save(model.state_dict(), f"/home/am/Documents/Software Development/\
-                       10_Academy Training/week_8-9/\
-                       fraud-detection/models/{model_name}.pth")
+            torch.save(model.state_dict(), f"{BASE_DIR}/models/{model_name}.pth")
         else:
-
             # Open the file in write binary mode ('wb')
-            with open(f"/home/am/Documents/Software Development/10_Academy Training/week_8-9/\
-                      fraud-detection/models/{model_name}.pkl", 'wb') as f:
-
-                # Pass the file object 'f' to pickle.dump
+            with open(f"{BASE_DIR}/{model_name}.pkl", 'wb') as f:
                 pickle.dump(model, f)
 
         print(f'Model {model_name} saved successfuly!')
 
     def track_versioning_experiment(self, model_name, accuracy, params=None):
-        """Tracking and versioning experiments.
-        """
-
         # Set the experiment (ideally should be done once, not every run)
         mlflow.set_experiment("Fraud Detection")
 
         with mlflow.start_run():
-
             # Log model name as a parameter
             mlflow.log_param("Model", model_name)
-
             # Log accuracy as a metric
             mlflow.log_metric("Accuracy", accuracy)
 
